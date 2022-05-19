aa7e1efcc910dc784dd7d62f7528097eb8951692
Yonatan <yonatan5556201@gmail.com>
2022-05-19 21:18:54 +0300

    added pickme and logic.py

---
+++ backend/dal/logic.py
@@ -0,0 +1,36 @@
+import json
+import random
+
+from backend.dal.wardrobe import get_all_items
+
+
+def pickme():
+    data = get_all_items()
+    shirts = []
+    pants = []
+    what_goes = {"red": ["black"],
+                 "blue": ["white", "black"],
+                 "green": ["white", "black"],
+                 "purple": ["white", "black"],
+                 "yellow": ["white", "black"],
+                 "white": ["purple", "yellow", "blue", "black", ]}
+    for item in data:
+        if item["category"] == "shirt":
+            shirts.insert(item)
+        if item["category"] == "pants":
+            pants.insert(item)
+    picked_shirt = random.choice(shirts)
+    filter1_pants = []
+    for item in pants:
+        if item["color"] == "black":
+            filter1_pants.insert(item)
+        else:
+            for col in what_goes[picked_shirt["color"]]:
+                if col == item["color"]:
+                    filter1_pants.insert(item)
+    possibro_pants = []
+    for item in filter1_pants:
+        if item["is_business"] == picked_shirt["is_business"] and item["weather"] == picked_shirt["weather"] and \
+                item["is_business"] == picked_shirt["is_business"]:
+            possibro_pants.insert(item)
+    return picked_shirt, random.choice(possibro_pants)
